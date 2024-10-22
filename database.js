00b07ebeb57b1a2656a8ace597d8f66a66086706
yzkoh2 <yongzhao249@gmail.com>
2024-10-22 11:34:20 +0800

    Update api

--- database.js
+++ database.js
@@ -2,20 +2,11 @@
   // Import the functions you need from the SDKs you need
   import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-app.js";
   import { getFirestore, collection, getDocs, getDoc, doc, setDoc, Timestamp } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-firestore.js";
-  import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-auth.js";
- 
+  import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-auth.js";
+  import { firebaseConfig } from "./configure.js";
   // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
 
-  // Your web app's Firebase configuration
-  const firebaseConfig = {
-    apiKey: "AIzaSyCQaBzoODvEpUiqunlogkNjne8L-8UNZJk",
-    authDomain: "wad2proj-f105e.firebaseapp.com",
-    projectId: "wad2proj-f105e",
-    storageBucket: "wad2proj-f105e.appspot.com",
-    messagingSenderId: "154785765446",
-    appId: "1:154785765446:web:765ccaaa71d2f5b68b5247"
-  };
 
   // Initialize Firebase
   export const app = initializeApp(firebaseConfig);
@@ -145,6 +136,7 @@
       throw new Error("User details could not be saved." + error.message);
     }
   }
+
 
 
 
