f7f1b064a03eb6dd96d184d30878f9a1971b9e6a
imranashryy <imranashry@gmail.com>
2024-10-30 20:58:04 +0800

    New horizontal listing in newHome.html. Added swiper-slider-3d-coverflow file. added smooth-scroll-complete.js for future use

--- main.js
+++ main.js
@@ -30,39 +30,39 @@ $(document).ready(function ($) {
         },
     });
 
-    var team_slider = new Swiper(".team-slider", {
-        slidesPerView: 3,
-        spaceBetween: 30,
-        loop: true,
-        autoplay: {
-            delay: 3000,
-            disableOnInteraction: false,
-        },
-        speed: 2000,
+    // var team_slider = new Swiper(".team-slider", {
+    //     slidesPerView: 3,
+    //     spaceBetween: 30,
+    //     loop: true,
+    //     autoplay: {
+    //         delay: 3000,
+    //         disableOnInteraction: false,
+    //     },
+    //     speed: 2000,
 
-        navigation: {
-            nextEl: ".swiper-button-next",
-            prevEl: ".swiper-button-prev",
-        },
-        pagination: {
-            el: ".swiper-pagination",
-            clickable: true,
-        },
-        breakpoints: {
-            0: {
-                slidesPerView: 1.2,
-            },
-            768: {
-                slidesPerView: 2,
-            },
-            992: {
-                slidesPerView: 3,
-            },
-            1200: {
-                slidesPerView: 3,
-            },
-        },
-    });
+    //     navigation: {
+    //         nextEl: ".swiper-button-next",
+    //         prevEl: ".swiper-button-prev",
+    //     },
+    //     pagination: {
+    //         el: ".swiper-pagination",
+    //         clickable: true,
+    //     },
+    //     breakpoints: {
+    //         0: {
+    //             slidesPerView: 1.2,
+    //         },
+    //         768: {
+    //             slidesPerView: 2,
+    //         },
+    //         992: {
+    //             slidesPerView: 3,
+    //         },
+    //         1200: {
+    //             slidesPerView: 3,
+    //         },
+    //     },
+    // });
 
     // new
     $(".main-navigation .nav-link").on("click", function(e) {
